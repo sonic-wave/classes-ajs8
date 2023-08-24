cf69ddfe1f0efcf31276a632bb54f1d72004e9a2
Ivan Khokhlov <mozhno@MacBook-Air-Mozhno.local>
2023-08-24 20:19:43 +0300

    change classes and tests

--- src/Classes/Character.js
+++ src/Classes/Character.js
@@ -1,16 +1,18 @@
 export default class Character {
-  constructor(name, type, health, level, attack, defence) {
+  constructor(name, type, health = 100, level = 1) {
+    const types = ['Bowerman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+
     if (name.length < 2 || name.length > 10 || typeof name !== 'string') {
       throw new Error('Имя должно быть строкой и от 2-х до 10-ти символов в длину');
     }
-    if (type !== 'Bowerman' && type !== 'Swordsman' && type !== 'Magician' && type !== 'Daemon' && type !== 'Undead' && type !== 'Zombie') {
-      throw new Error('Тип должен быть равен одному из следующих типов: Bowman, Swordsman, Magician, Daemon, Undead, Zombie');
+    if (!types.includes(type)) {
+      throw new Error(`Тип должен быть равен одному из следующих типов: ${types.join(', ')}`);
     }
     this.name = name;
     this.type = type;
     this.health = health;
     this.level = level;
-    this.attack = attack;
-    this.defence = defence;
+    this.attack = undefined;
+    this.defence = undefined;
   }
 }
